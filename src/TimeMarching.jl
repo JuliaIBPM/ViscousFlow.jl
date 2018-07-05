9f57a1f830332169626db11eeaf1d73e43c29a16
Jeff Eldredge <jdeldre@g.ucla.edu>
2018-07-05 14:21:59 -0700

    Added Euler for RK option

--- src/TimeMarching.jl
+++ src/TimeMarching.jl
@@ -25,6 +25,8 @@ const RK31 = RKParams{3}([0.5, 1.0, 1.0],
                        √3/3 (3-√3)/3        0
                        (3+√3)/6    -√3/3 (3+√3)/6])
 
+const Euler = RKParams{1}([1.0],ones(1,1))
+
 struct IFHERK{NS,FH,FB1,FB2,FR1,FR2,TU,TF}
 
   # time step size
@@ -32,7 +34,7 @@ struct IFHERK{NS,FH,FB1,FB2,FR1,FR2,TU,TF}
 
   rk :: RKParams
 
-  # Integrating factor
+  # Integrating factors
   H :: Vector{FH}
 
   B₁ᵀ :: FB1  # operates on TF and returns TU
@@ -40,11 +42,13 @@ struct IFHERK{NS,FH,FB1,FB2,FR1,FR2,TU,TF}
   r₁ :: FR1  # function of u and t, returns TU
   r₂ :: FR2  # function of t, returns TF
 
+  # Saddle-point systems
   S :: Vector{SaddleSystem}  # -B₂HB₁ᵀ
 
   # scratch space
   qᵢ :: TU
   w :: Vector{TU}
+  fbuffer :: TF
 
   # flags
   _issymmetric :: Bool
@@ -81,6 +85,7 @@ function (::Type{IFHERK})(u::TU,f::TF,Δt::Float64,
     # scratch space
     qᵢ = deepcopy(u)
     w = [deepcopy(u) for i = 1:NS-1]
+    fbuffer = deepcopy(f)
 
     dclist = diff([0;rk.c])
 
@@ -97,7 +102,7 @@ function (::Type{IFHERK})(u::TU,f::TF,Δt::Float64,
 
     IFHERK{NS,htype,FB1,FB2,FR1,FR2,TU,TF}(Δt,rk,
                                 H,B₁ᵀ,B₂,r₁,r₂,S,
-                                qᵢ,w,
+                                qᵢ,w,fbuffer,
                                 issymmetric)
 end
 
@@ -110,45 +115,51 @@ end
 
 # Advance the IFHERK solution by one time step
 function (scheme::IFHERK{NS,FH,FB1,FB2,FR1,FR2,TU,TF})(t::Float64,u::TU,f::TF) where {NS,FH,FB1,FB2,FR1,FR2,TU,TF}
-  @get scheme (Δt,rk,H,S,B₁ᵀ,B₂,r₁,r₂,qᵢ,w)
+  @get scheme (Δt,rk,H,S,B₁ᵀ,B₂,r₁,r₂,qᵢ,w,fbuffer)
 
   # H[i] corresponds to H(i,i+1) = H((cᵢ - cᵢ₋₁)Δt)
 
-  # first stage, i = 1
   i = 1
-  tᵢ₊₁ = t + Δt*rk.c[i]
   qᵢ .= u
-  w[i] .= Δt*rk.a[i,i]*r₁(u,tᵢ₊₁) # gᵢ
-  u .+= w[i] # r₁ = qᵢ + gᵢ
-  fbuffer .= r₂(tᵢ₊₁) # r₂
-  A_ldiv_B!((u,f),S[i],(u,fbuffer))  # solve saddle point system
 
-  # diffuse the scratch vectors
-  H[i](qᵢ,qᵢ) # qᵢ₊₁ = H(i,i+1)qᵢ
-  H[i](w[i],w[i]) # H(i,i+1)gᵢ
-
-  # stages 2 through NS-1
-  for i = 2:NS-1
+  if NS > 1
+    # first stage, i = 1
     tᵢ₊₁ = t + Δt*rk.c[i]
-    w[i-1] .= (w[i-1]-S[i-1].A⁻¹B₁ᵀf)/(Δt*rk.a[i-1,i-1]) # w(i,i-1)
+
     w[i] .= Δt*rk.a[i,i]*r₁(u,tᵢ₊₁) # gᵢ
-    u .= qᵢ .+ w[i] # r₁
-    for j = 1:i-1
-      u .+= Δt*rk.a[i,j]*w[j] # r₁
-    end
+    u .+= w[i] # r₁ = qᵢ + gᵢ
     fbuffer .= r₂(tᵢ₊₁) # r₂
-    A_ldiv_B!((u,f),S[i],(u,fbuffer)) # solve saddle point system
+    A_ldiv_B!((u,f),S[i],(u,fbuffer))  # solve saddle point system
 
     # diffuse the scratch vectors
     H[i](qᵢ,qᵢ) # qᵢ₊₁ = H(i,i+1)qᵢ
-    for j = 1:i
-      H[i](w[j],w[j]) # for j = i, this sets H(i,i+1)gᵢ
-    end
+    H[i](w[i],w[i]) # H(i,i+1)gᵢ
 
+    # stages 2 through NS-1
+    for i = 2:NS-1
+      tᵢ₊₁ = t + Δt*rk.c[i]
+      w[i-1] .= (w[i-1]-S[i-1].A⁻¹B₁ᵀf)/(Δt*rk.a[i-1,i-1]) # w(i,i-1)
+      w[i] .= Δt*rk.a[i,i]*r₁(u,tᵢ₊₁) # gᵢ
+      u .= qᵢ .+ w[i] # r₁
+      for j = 1:i-1
+        u .+= Δt*rk.a[i,j]*w[j] # r₁
+      end
+      fbuffer .= r₂(tᵢ₊₁) # r₂
+      A_ldiv_B!((u,f),S[i],(u,fbuffer)) # solve saddle point system
+
+      # diffuse the scratch vectors
+      H[i](qᵢ,qᵢ) # qᵢ₊₁ = H(i,i+1)qᵢ
+      for j = 1:i
+        H[i](w[j],w[j]) # for j = i, this sets H(i,i+1)gᵢ
+      end
+
+    end
+    i = NS
+    w[i-1] .= (w[i-1]-S[i-1].A⁻¹B₁ᵀf)/(Δt*rk.a[i-1,i-1]) # w(i,i-1)
   end
-  i = NS
+
+  # final stage (assembly)
   tᵢ₊₁ = t + Δt*rk.c[i]
-  w[i-1] .= (w[i-1]-S[i-1].A⁻¹B₁ᵀf)/(Δt*rk.a[i-1,i-1]) # w(i,i-1)
   u .= qᵢ .+ Δt*rk.a[i,i]*r₁(u,tᵢ₊₁) # r₁
   for j = 1:i-1
     u .+= Δt*rk.a[i,j]*w[j] # r₁
