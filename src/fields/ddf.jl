f174e5e5f6a72b11f625fe6214b257c3fadbd00e
Jeff Eldredge <jdeldre@g.ucla.edu>
2018-07-06 15:25:11 -0700

    Streamlined the ddf code

--- src/fields/ddf.jl
+++ src/fields/ddf.jl
@@ -51,84 +51,26 @@ macro ddffunc(ddftype)
                           $fname(abs(x)/DX)/DX*$fname(abs(y)/DX)/DX
             (::DDF{$ddftype,DX})(x::T,y::T,z::T) where {DX,T <: Real} =
                           $fname(abs(x)/DX)/DX*$fname(abs(y)/DX)/DX*$fname(abs(z)/DX)/DX
-            #=
-            (::DDF{$ddftype,DX})(x::AbstractVector{T}) where {DX,T <: Real} =
-                          $fname.(abs.(x)/DX)
-            (::DDF{$ddftype,DX})(x::AbstractVector{T},
-                                 y::AbstractVector{T}) where {DX,T <: Real} =
-                          $fname.(abs.(x)/DX).*$fname.(abs.(y)/DX)
-            (::DDF{$ddftype,DX})(x::AbstractVector{T},
-                                 y::AbstractVector{T},
-                                 z::AbstractVector{T}) where {DX,T <: Real} =
-                          $fname.(abs.(x)/DX).*$fname.(abs.(y)/DX).*$fname.(abs.(z)/DX)
-            =#
             end)
 end
 
 @ddffunc Roma
 
-roma1(r) = (1+sqrt(-3r^2+1))/3;
-roma2(r) = (5-3r-sqrt(1-3(1-r)^2))/6;
+roma1(r) = (1+sqrt(-3r^2+1))/3
+roma2(r) = (5-3r-sqrt(1-3(1-r)^2))/6
 
-function ddf_roma(r::Real)
-    rr = abs(r)
-    if rr > 1.5
-      return 0.0
-    end
-    if rr <= 0.5
-      return roma1(rr)
-    else
-      return roma2(rr)
-    end
-end
+ddf_roma(r::Real) = r > 1.5 ? 0.0 : r <= 0.5 ? roma1(r) : roma2(r)
 
-
-
-#=
-function ddf_roma(r::Array{Float64,1})
-  rr = abs.(r)
-
-  f = zeros(rr)
-  pts = find(x -> x<=0.5,rr)
-  f[pts] = roma1.(rr[pts])
-
-  pts = find(x -> 0.5 < x < 1.5,rr)
-  f[pts] = roma2.(rr[pts])
-
-  f
-
-end
-=#
 
 @ddffunc Goza
 
-goza(r) = exp(-π^2/36 * r^2)*sqrt(π/36);
+ddf_goza(r::Real) = r > 14 ? 0.0 : exp(-π^2/36 * r^2)*sqrt(π/36)
 
-function ddf_goza(r::Real)
-  rr = abs(r)
-  return rr > 14 ? 0.0 : goza(rr)
-end
-
-#=
-function ddf_goza(r::Array{Float64,1})
-  rr = abs.(r)
-
-  f = zeros(rr)
-  pts = find(x -> x<=14,rr)
-  f[pts] = goza.(rr[pts])
-
-  f
-end
-=#
 
 @ddffunc Witchhat
 
-witchhat(r) = 1-r;
+ddf_witchhat(r::Real) = r > 1 ? 0.0 : 1-r
 
-function ddf_witchhat(r::Real)
-  rr = abs(r)
-  return rr > 1 ? 0.0 : witchhat(rr)
-end
 
 function Base.show(io::IO, ddf::DDF{C,DX}) where {C<:DDFType,DX}
     print(io, "Discrete delta function operator of type $C, with spacing $DX")
