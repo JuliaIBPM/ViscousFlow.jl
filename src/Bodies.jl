3cddb46690c493b3f88ab2b311ba2eb8764db55a
Jeff Eldredge <jdeldre@g.ucla.edu>
2018-08-11 14:20:55 -0700

    Added NACA4 shape to Bodies

--- src/Bodies.jl
+++ src/Bodies.jl
@@ -256,6 +256,137 @@ function Base.show(io::IO, body::Plate{N}) where {N}
     println(io, "   Current angle (rad): $(body.α)")
 end
 
+"""
+    NACA4(cam,pos,thick[;np=20][,Zc=0.0+0.0im][,len=1.0]) -> Vector{Complex128}
+
+Generates the vertices of a NACA 4-digit airfoil of chord length 1. The
+relative camber is specified by `cam`, the position of
+maximum camber (as fraction of chord) by `pos`, and the relative thickness
+by `thick`.
+
+The optional parameter `np` specifies the number of points on the upper
+or lower surface. The optional parameter `Zc` specifies the mean position of
+the vertices (which is set to the origin by default). The optional parameter
+`len` specifies the chord length.
+
+# Example
+
+```jldoctest
+julia> w = naca4(0.0,0.0,0.12);
+
+julia> p = Polygon(w);
+```
+"""
+mutable struct NACA4{N} <: Body{N}
+  camber :: Float64
+  pos :: Float64
+  thick :: Float64
+
+  cent :: Tuple{Float64,Float64}
+  α :: Float64
+
+  x̃ :: Vector{Float64}
+  ỹ :: Vector{Float64}
+
+  x :: Vector{Float64}
+  y :: Vector{Float64}
+
+end
+
+
+function NACA4(cam::Number,pos::Number,t::Number;np=20,Zc=0.0+0.0im,len=1.0)
+
+# Here, cam is the fractional camber, pos is the fractional chordwise position
+# of max camber, and t is the fractional thickness.
+
+npan = 2*np-2
+
+# Trailing edge bunching
+an = 1.5
+anp = an+1
+x = zeros(np)
+
+θ = zeros(size(x))
+yc = zeros(size(x))
+
+for j = 1:np
+    frac = Float64((j-1)/(np-1))
+    x[j] = 1 - anp*frac*(1-frac)^an-(1-frac)^anp;
+    if x[j] < pos
+        yc[j] = cam/pos^2*(2*pos*x[j]-x[j]^2)
+        if pos > 0
+            θ[j] = atan(2*cam/pos*(1-x[j]/pos))
+        end
+    else
+        yc[j] = cam/(1-pos)^2*((1-2*pos)+2*pos*x[j]-x[j]^2)
+        if pos > 0
+            θ[j] = atan(2*cam*pos/(1-pos)^2*(1-x[j]/pos))
+        end
+    end
+end
+
+xu = zeros(size(x))
+yu = xu
+xl = xu
+yl = yu
+
+yt = t/0.20*(0.29690*sqrt.(x)-0.12600*x-0.35160*x.^2+0.28430*x.^3-0.10150*x.^4)
+
+xu = x-yt.*sin.(θ)
+yu = yc+yt.*cos.(θ)
+
+xl = x+yt.*sin.(θ)
+yl = yc-yt.*cos.(θ)
+
+rt = 1.1019*t^2;
+θ0 = 0
+if pos > 0
+    θ0 = atan(2*cam/pos)
+end
+# Center of leading edge radius
+xrc = rt*cos(θ0)
+yrc = rt*sin(θ0)
+θle = collect(0:π/50:2π)
+xlec = xrc+rt*cos.(θle)
+ylec = yrc+rt*sin.(θle)
+
+# Assemble data
+coords = [xu yu xl yl x yc]
+cole = [xlec ylec]
+
+# Close the trailing edge
+xpanold = [0.5*(xl[np]+xu[np]); flipdim(xl[2:np-1],1); xu[1:np-1]]
+ypanold = [0.5*(yl[np]+yu[np]); flipdim(yl[2:np-1],1); yu[1:np-1]]
+
+xpan = zeros(npan)
+ypan = zeros(npan)
+for ipan = 1:npan
+    if ipan < npan
+        xpan1 = xpanold[ipan]
+        ypan1 = ypanold[ipan]
+        xpan2 = xpanold[ipan+1]
+        ypan2 = ypanold[ipan+1]
+    else
+        xpan1 = xpanold[npan]
+        ypan1 = ypanold[npan]
+        xpan2 = xpanold[1]
+        ypan2 = ypanold[1]
+    end
+    xpan[ipan] = 0.5*(xpan1+xpan2)
+    ypan[ipan] = 0.5*(ypan1+ypan2)
+end
+w = Complex128[1;flipdim(xpan,1)+im*flipdim(ypan,1)]*len
+w -=mean(w)
+
+x̃ = real.(w)
+ỹ = imag.(w)
+
+
+NACA4{length(x̃)}(cam,pos,t,(0.0,0.0),0.0,x̃,ỹ,x̃,ỹ)
+
+end
+
+
 #=
 export Body
 
