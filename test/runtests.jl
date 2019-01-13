cee71ea6a18771b248f198b107d6cd98d13b0288
Jeff Eldredge <jdeldre@g.ucla.edu>
2019-01-13 15:34:52 -0800

    Passes all tests in 0.6 and 0.7

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,11 +10,11 @@ using Whirl
 
 #@test isempty(detect_ambiguities(Whirl))
 
-#include("fields.jl")
-#include("points.jl")
-#include("timemarching.jl")
+include("fields.jl")
+include("points.jl")
+include("timemarching.jl")
 include("saddle.jl")
-#include("systems.jl")
+include("systems.jl")
 
 
 #@testset ExtendedTestSet "All tests" begin
