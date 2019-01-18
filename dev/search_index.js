e3153ad5b567000ca42f2083286801a8bfc551c5
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-01-18 17:45:39 +0000

    build based on f96d1d4

--- dev/search_index.js
+++ dev/search_index.js
@@ -21,7 +21,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "Installation",
     "category": "section",
-    "text": "This package works on Julia 0.6, 0.7 and 1.0. To install, simply runjulia> using Pkg\n\njulia> Pkg.add(\"https://github.com/jdeldre/ViscousFlow.jl\")in the Julia REPL. Since this package is still under heavy development, you should runjulia> Pkg.test(\"Whirl\") # might take some timeto make sure things are working as intended andjulia> Pkg.update()to get the most recent version of the library and its dependencies.The plots in this documentation are generated using Plots.jl. You might want to install that too to follow the examples."
+    "text": "This package works on Julia 0.6, 0.7 and 1.0. To install, simply runjulia> using Pkg\n\njulia> Pkg.add(\"https://github.com/jdeldre/ViscousFlow.jl\")in the Julia REPL. Since this package is still under heavy development, you should runjulia> Pkg.test(\"ViscousFlow\") # might take some timeto make sure things are working as intended andjulia> Pkg.update()to get the most recent version of the library and its dependencies.The plots in this documentation are generated using Plots.jl. You might want to install that too to follow the examples."
 },
 
 {
