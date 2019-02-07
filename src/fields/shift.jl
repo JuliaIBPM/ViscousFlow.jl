0b3204dac8561a88d5fdc784bc33dc95714ca60d
Jeff Eldredge <jdeldre@g.ucla.edu>
2019-02-07 15:42:38 -0800

    Added cellshift from primal nodes to primal edges

--- src/fields/shift.jl
+++ src/fields/shift.jl
@@ -131,6 +131,23 @@ function cellshift!(primal::Tuple{Nodes{Primal, NX, NY},Nodes{Primal, NX, NY}}, 
     primal
 end
 
+"""
+    cellshift!(q::Edges{Primal},w::Nodes{Primal})
+
+Shift (by linear interpolation) the primal nodal data `w` to the edges of the primal cells,
+and return the result in `q`.
+"""
+function cellshift!(primal::Edges{Primal, NX, NY}, w::Nodes{Primal,NX, NY}) where {NX, NY}
+    @inbounds for y in 1:NY-1, x in 2:NX-1
+        primal.u[x,y] = (w[x-1,y] + w[x,y])/2
+    end
+
+    @inbounds for y in 2:NY-1, x in 1:NX-1
+        primal.v[x,y] = (w[x,y-1] + w[x,y])/2
+    end
+    primal
+end
+
 
 # I don't like this one. It is ambiguous what type of nodes are being shifted to.
 nodeshift(edges::Edges{Primal,NX,NY}) where {NX,NY} = cellshift!((Nodes(Dual, (NX,NY)),Nodes(Dual, (NX,NY))),edges)
