cde67a33b91fa730ecbf5e267cca6b05be90a30c
Jeff Eldredge <jdeldre@g.ucla.edu>
2019-10-06 22:28:50 -0700

    Fixed some bugs in utils

--- src/systems/utils.jl
+++ src/systems/utils.jl
@@ -3,11 +3,12 @@ import JLD: save, load
 
 export WritePlan,StorePlan, initialize_storage, store_data!
 
+
 """
     WritePlan(file,write_Δt,varlist)
 
 Create a plan for writing data to file. The filename `file` is specified,
-to be written to every `write_Δt` time units. The variables to be written are
+to be written to every `write_Δt` time units. The variable names to be written are
 specified as a vector of strings in `varlist`.
 """
 struct WritePlan
@@ -17,15 +18,17 @@ struct WritePlan
 end
 
 """
-    save(t,R::WritePlan)
+    save(t,R::WritePlan,v)
 
 Check if time `t` is appropriate for writing to file, according to the WritePlan `R`,
-and if so, write the specified variables in `R` to the file in `R`.
+and if so, write the specified variables `v` (which may be separated by commas)
+in `R` to the file in `R`.
 """
-function save(t,R::WritePlan)
-    if (isapprox(mod(t,R.restart_Δt),0,atol=1e-6) ||
-        isapprox(mod(t,R.restart_Δt),R.restart_Δt,atol=1e-6))
-        save(R.filen,writelist(R)...)
+function save(t,R::WritePlan,v...)
+    tol = 1e-8
+    if (isapprox(mod(t,R.write_Δt),0,atol=tol) ||
+        isapprox(mod(t,R.write_Δt),R.write_Δt,atol=tol))
+        save(R.filen,_writelist(R,v)...)
     end
 end
 
@@ -36,29 +39,36 @@ Load the data stored in the file specified by the WritePlan `R`.
 """
 load(R::WritePlan) = load(R.filen)
 
-function _writelist(R::WritePlan)
+function _writelist(R::WritePlan,v)
     list = ()
-    for v in R.varlist
-        list = (list...,v,eval(Symbol(v)))
+    @assert length(R.varlist)==length(v)
+    for (i,vname) in enumerate(R.varlist)
+        list = (list...,vname,v[i])
     end
     return list
 end
 
 
+
+
 """
-    StorePlan(min_t,max_t,store_Δt,varlist)
+    StorePlan(min_t,max_t,store_Δt,v)
 
 Create a plan for storing history data. The storage of data is specified to
 start at time unit `min_t` and to proceed until (and including) `max_t`, and
 is stored every `store_Δt` time units. The list of variables to be stored is
-specified as a vector of strings in `varlist`. Tuple-type variables are unwrapped
+specified as a list of variables `v`. Tuple-type variables are unwrapped
 into separate storage.
 """
 struct StorePlan
     min_t::Float64
     max_t::Float64
     store_Δt::Float64
-    varlist::Vector{String}
+    varlist::Vector{DataType}
+end
+
+function StorePlan(min_t,max_t,store_Δt,v)
+    return StorePlan(min_t,max_t,store_Δt,_get_type(v))
 end
 
 """
@@ -67,20 +77,14 @@ end
 Initialize a storage data stack for the storage plan `S`. The output is
 an empty vector of vectors.
 """
-function initialize_storage(R::StorePlan)
+function initialize_storage(S::StorePlan)
     data = []
-    for (i,v) in enumerate(R.varlist)
-        vtype = typeof(eval(Symbol(v)))
-        if vtype <: Tuple
-            for vi in eval(Symbol(v))
-                push!(data,typeof(vi)[])
-            end
-        else
-            push!(data,vtype[])
-        end
+    for v in S.varlist
+        push!(data,v[])
     end
     return data
 end
+
 
 """
     store_data!(data,t,S::StorePlan)
@@ -88,22 +92,46 @@ end
 Check whether time `t` is a time for saving for storage as described by plan `S`,
 and if so, push the variables specified in `S` onto the `data` stack.
 """
-function store_data!(data,t,R::StorePlan)
-    if t >= (R.min_t-1e-6) && t <= (R.max_t + 1e-6) &&
-        ((isapprox(mod(t,R.store_Δt),0,atol=1e-6) ||
-          isapprox(mod(t,R.store_Δt),R.store_Δt,atol=1e-6)))
-        cnt = 0
-        for v in R.varlist
-            if typeof(eval(Symbol(v))) <: Tuple
-                for vi in eval(Symbol(v))
-                    cnt += 1
-                    push!(data[cnt],vi)
-                end
-            else
-                cnt += 1
-                push!(data[cnt],eval(Symbol(v)))
-            end
-        end
+function store_data!(data,t,S::StorePlan,v...)
+  tol = 1e-8
+  if t >= (R.min_t-tol) && t <= (R.max_t + tol) &&
+      ((isapprox(mod(t,R.store_Δt),0,atol=tol) ||
+        isapprox(mod(t,R.store_Δt),R.store_Δt,atol=tol)))
+        store_data!(data,S::StorePlan,v)
+  end
+  return data
+end
+
+
+function _get_type(u)
+    tlist = DataType[]
+    _get_type!(tlist,u)
+    return tlist
+end
+function _get_type!(tlist,u::Tuple)
+    for ui in u
+        _get_type!(tlist,ui)
     end
+    return tlist
+end
+function _get_type!(tlist,u)
+    push!(tlist,typeof(u))
+    return tlist
+end
+
+function store_data!(data,S::StorePlan,u)
+    cnt = [0]
+    return _store_data!(data,cnt,S,u)
+end
+function _store_data!(data,cnt,S::StorePlan,u::Tuple)
+    for ui in u
+        _store_data!(data,cnt,S,ui)
+    end
+    return data
+end
+function _store_data!(data,cnt,S::StorePlan,u)
+    cnt[1] += 1
+    @assert S.varlist[cnt[1]] == typeof(u)
+    push!(data[cnt[1]],u)
     return data
 end
