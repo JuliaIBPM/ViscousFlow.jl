a703456c70610b0f0f6ab06053e67e8dc001112a
Jeff Eldredge <jdeldre@g.ucla.edu>
2020-04-27 23:01:07 -0700

    Added parameter to Nodes type for the data field. Enhanced inner constructors to allow for vector inputs that are reshaped into matrix of appropriate size.

--- src/fields/nodes.jl
+++ src/fields/nodes.jl
@@ -1,5 +1,13 @@
 import Base: size, ∘
 
+#=
+To do:
+* Add a parameter of all GridData types that describes the type of the data array
+  This will allow the data field to be of other AbstractMatrix types, like
+  reshaped array
+* Create constructors for all GridData types that accept an input vector
+  and reshape this into an array (or arrays, via view, for Edges)
+=#
 
 """
     Nodes{Dual/Primal}
@@ -15,8 +23,10 @@ and allows the use of [size].
 - `Nodes(C,w)` performs the same construction, but uses existing field data `w`
   of `Nodes` type to determine the size of the grid.
 """
-struct Nodes{C <: CellType, NX, NY, T <: Number} <: ScalarGridData{NX,NY,T}
-    data::Matrix{T}
+struct Nodes{C <: CellType, NX, NY, T <: Number, DT <: AbstractMatrix} <: ScalarGridData{NX,NY,T}
+    data::DT
+    Nodes{C,NX,NY,T,DT}(data::AbstractMatrix) where {C<: CellType,NX,NY,T<:Number,DT} = new{C,NX,NY,T,typeof(data)}(data)
+    Nodes{C,NX,NY,T,DT}(data::AbstractVector) where {C<: CellType,NX,NY,T<:Number,DT} = (u = reshape(data,node_inds(C,(NX,NY))); new{C,NX,NY,T,typeof(u)}(u))
 end
 
 # Number of indices
@@ -28,7 +38,7 @@ node_inds(::Type{Primal}, dualnodedims) = (dualnodedims[1]-1, dualnodedims[2]-1)
 
 function Nodes(T::Type{C}, dualnodedims::Tuple{Int, Int};dtype=Float64) where {C <: CellType}
     dims = node_inds(T, dualnodedims)
-    Nodes{T, dualnodedims...,dtype}(zeros(dtype,dims))
+    Nodes{T, dualnodedims...,dtype,typeof(zeros(dtype,dims))}(zeros(dtype,dims))
 end
 
 # This allows easy construction of nodes of either type from existing nodes of either
@@ -37,11 +47,12 @@ Nodes(C, ::GridData{NX,NY,T};dtype=T) where {NX, NY,T <: Number} = Nodes(C, (NX,
 
 
 Nodes(C, nx::Int, ny::Int;dtype=Float64) = Nodes(C,(nx,ny),dtype=dtype)
-(::Type{Nodes{C,NX,NY,T}})() where {C,NX,NY,T} = Nodes(C, (NX, NY),dtype=T)
+(::Type{Nodes{C,NX,NY,T,DT}})() where {C,NX,NY,T,DT} = Nodes(C, (NX, NY),dtype=T)
 
-Base.similar(::Nodes{C,NX,NY,T};element_type=T) where {C,NX,NY,T} = Nodes(C, (NX, NY),dtype=element_type)
 
-function Base.show(io::IO, nodes::Nodes{C, NX, NY,T}) where {C, NX, NY, T}
+Base.similar(::Nodes{C,NX,NY,T,DT};element_type=T) where {C,NX,NY,T,DT} = Nodes(C, (NX, NY),dtype=element_type)
+
+function Base.show(io::IO, nodes::Nodes{C, NX, NY,T,DT}) where {C, NX, NY, T, DT}
     nodedims = "(nx = $NX, ny = $NY)"
     dims = "(nx = $(size(nodes,1)), ny = $(size(nodes,2)))"
     println(io, "$C nodes in a $nodedims cell grid of type $T data")
