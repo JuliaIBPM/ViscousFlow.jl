ba0b715b8e7f5b69d361a8510703169af1e9fd34
Jeff Eldredge <jdeldre@g.ucla.edu>
2020-06-20 20:12:52 -0700

    Updated the main file

--- src/ViscousFlow.jl
+++ src/ViscousFlow.jl
@@ -6,21 +6,14 @@ module ViscousFlow
 
 using Reexport
 
+@reexport using CartesianGrids
+@reexport using RigidBodyTools
+@reexport using ConstrainedSystems
 
 #== Imports/Exports ==#
 
-include("Utils.jl")
-@reexport using .Utils
-
-@reexport using CartesianGrids
-
-@reexport using RigidBodyTools
-
-@reexport using ConstrainedSystems
-
-
-include("Systems.jl")
-
+include("utils.jl")
+include("systems.jl")
 
 
 #== Plot Recipes ==#
