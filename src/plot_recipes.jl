06a183767de270839205a113e6593afa9c0d036d
Jeff Eldredge <jdeldre@g.ucla.edu>
2021-01-10 16:51:38 -0800

    Fixed an error in NavierStokes call with static points. Updated the plot recipe for vector grid data

--- src/plot_recipes.jl
+++ src/plot_recipes.jl
@@ -5,16 +5,30 @@ import PlotUtils: cgrad
 
 
 @recipe function f(w::T,sys::NavierStokes) where {T<:GridData}
-    @series begin
-      trim := 2
-      w, sys.grid
-    end
+  @series begin
+    trim := 2
+    w, sys.grid
+  end
 
-    if !(isnothing(sys.bodies))
+  if !(isnothing(sys.bodies))
+    if typeof(w) <: ScalarGridData
       @series begin
         sys.bodies
       end
+    else
+      if typeof(w) <: VectorGridData
+        nsub = 2
+      elseif typeof(w) <: TensorGridData
+        nsub = 4
+      end
+      for i in 1:nsub
+        @series begin
+          subplot := i
+          sys.bodies
+        end
+      end
     end
+  end
 end
 
 @recipe function f(w1::T1,w2::T2,sys::NavierStokes) where {T1<:GridData,T2<:GridData}
