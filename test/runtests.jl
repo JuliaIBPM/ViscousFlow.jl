b5fa802c4795080f0377e073d3eaebac4e393606
Jeff Eldredge <jdeldre@g.ucla.edu>
2021-04-18 22:21:37 -0700

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,8 +18,8 @@ end
 if GROUP == "All" || GROUP == "Notebooks"
   for (root, dirs, files) in walkdir(litdir)
     for file in files
-      endswith(file,".jl") && startswith(file,"6") && Literate.notebook(joinpath(root, file),notebookdir)
-      #endswith(file,".jl") && Literate.notebook(joinpath(root, file),notebookdir)
+      #endswith(file,".jl") && startswith(file,"6") && Literate.notebook(joinpath(root, file),notebookdir)
+      endswith(file,".jl") && Literate.notebook(joinpath(root, file),notebookdir)
     end
   end
 end
