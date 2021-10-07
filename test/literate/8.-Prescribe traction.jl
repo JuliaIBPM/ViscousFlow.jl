fd23ef17513ce10b1b7069e3172a618105f09f58
Jeff Eldredge <jdeldre@g.ucla.edu>
2021-10-07 13:45:58 -0700

    Shortened example 8

--- test/literate/8.-Prescribe traction.jl
+++ test/literate/8.-Prescribe traction.jl
@@ -13,16 +13,16 @@ Re = 100
 
 #-
 Δx,Δt = setstepsizes(Re,gridRe=4)
-xlim, ylim = (-16,16),(-5,5)
+xlim, ylim = (-6,6),(-2,2)
 
 #=
 ## Create channel
-Two plates of length 30 centered at (0,0) are used to represent the wall of the pipe. All bodies must be put into a bodylist to create the Navier Stokes system. 
+Two plates of length 30 centered at (0,0) are used to represent the wall of the pipe. All bodies must be put into a bodylist to create the Navier Stokes system.
 =#
 channel = BodyList()
 tl = RigidTransformList()
-push!(channel,Plate(30,1.5Δx)); push!(tl,RigidTransform((0.0,1.0),0.));
-push!(channel,Plate(30,1.5Δx)); push!(tl,RigidTransform((0.0,-1.0),0.));
+push!(channel,Plate(10,1.5Δx)); push!(tl,RigidTransform((0.0,1.0),0.));
+push!(channel,Plate(10,1.5Δx)); push!(tl,RigidTransform((0.0,-1.0),0.));
 tl(channel)
 plot(channel)
 
@@ -31,23 +31,23 @@ plot(channel)
 The traction line source functions will set a specified traction strength onto a sets of specified location, so here, we use two veritcal plates with length of 2 (gap of between the wall) to represent the inlet and outlet. LineSourceParams( ) is used to collect the location of a body.
 =#
 inlet = Plate(2.0,1.5Δx)
-T = RigidTransform((-15.0,0.0),π/2)
+T = RigidTransform((-5.0,0.0),π/2)
 T(inlet)
 τin = LineSourceParams(inlet)
 TractionStrength_in = VectorData(collect(inlet))
 fill!(TractionStrength_in.u,-1.0)
 fill!(TractionStrength_in.v,0.0)
-
+nothing #hide
 #-
 
 outlet = Plate(2.0,1.5Δx)
-T = RigidTransform((15.0,0.0),π/2)
+T = RigidTransform((5.0,0.0),π/2)
 T(outlet)
 τout = LineSourceParams(outlet)
 TractionStrength_out = VectorData(collect(outlet))
 fill!(TractionStrength_out.u,0.0)
 fill!(TractionStrength_out.v,0.0)
-
+nothing #hide
 #-
 
 plot!(inlet)
@@ -55,27 +55,27 @@ plot!(outlet)
 
 #=
 ## Create N-S system
-Note the order of $\tau$ in $\tau$_bc is important for prescribing their strength 
+Note the order of $\tau$ in $\tau$_bc is important for prescribing their strength
 =#
-sys = NavierStokes(Re,Δx,xlim,ylim,Δt,channel,τ_bc=[τin,τout])
+sys = NavierStokes(Re,Δx,xlim,ylim,Δt,channel,τ_bc=[τin,τout]);
 
 #=
 Initialize and Integrate
 =#
 u0 = newstate(sys)
-tspan = (0.0,10.0)
-integrator = init(u0,tspan,sys)
+tspan = (0.0,12.0)
+integrator = init(u0,tspan,sys);
 
 #=
 ## Set traction strength for inlet and outlet
 Outlet is skipped because it is 0 by default.
 =#
-set_linesource_strength!(sys.τ_bc[1],TractionStrength_in)
+set_linesource_strength!(sys.τ_bc[1],TractionStrength_in);
 
 #=
 ## Solve
 =#
-step!(integrator,10.0)
+step!(integrator,12.0)
 
 #=
 ## Examine
