463a7ad7fea01063c672ac6e1f0638d4c991ded8
Jeff Eldredge <jdeldre@g.ucla.edu>
2024-10-20 11:25:34 -0700

    Updated literate file to use CG for solves on moving body

--- test/literate/6.-Viscous-flow-about-a-moving-body.jl
+++ test/literate/6.-Viscous-flow-about-a-moving-body.jl
@@ -184,13 +184,17 @@ Umax, imax, tmax, bmax = maxvelocity(u0,sys)
 L = 3*Lp
 Re_eff = my_params["Re"]*Umax*L
 
-#-
+#=
+In other problems, we have stored the inverse of the matrix system that we
+have to solve at each time stage. But since that matrix system changes
+as the body moves, it is faster here to use an iterative solver (congjugate gradient)
+=#
 tspan = (0.0,10.0)
-integrator = init(u0,tspan,sys)
+integrator = init(u0,tspan,sys,alg=LiskaIFHERK(saddlesolver=CG))
 
 #=
 ### Solve
-This takes longer per time step than it does for stationary bodies. Here, we only
+This takes a bit longer per time step than it does for stationary bodies. Here, we only
 run it for 1.5 time units just to demonstrate it.
 =#
 @time step!(integrator,1.5)
